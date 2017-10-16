e3d141958940a38313351e20db7ad77b8fe8ddcb
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-16 18:17:00 +0700

    add error status in middlewares

--- middlewares/anyRoleAccess.js
+++ middlewares/anyRoleAccess.js
@@ -4,17 +4,19 @@ module.exports = (req, res, next) => {
 	if (req.headers.hasOwnProperty('token')) {
 		jwt.verify(req.headers.token, process.env.SECRET_KEY, (err, decoded) => {
 			if (err) {
-				res.send(err);
+				res.status(500).send(err);
 			} else if (['admin', 'user'].indexOf(decoded.role) !== -1) { 
 				next();
 			} else {
 				res.status(401).send({
+					status: 401;
 					message: 'Unauthorized: You do not have access to this feature. Sign up/Sign in.'
 				});
 			}
 		});
 	} else {
 		res.status(401).send({
+			status: 401;
 			message: 'Unauthorized: You do not have access to this feature. Sign up/Sign in.'
 		});
 	}
