4bd5fbcc31c85921c813399ce5b60ebb7f03a194
Dimitri <deathmitri@gmail.com>
2017-10-16 12:09:40 +0700

    Finished users API

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,16 +4,77 @@ class UserController {
 
   static getAll (req, res) {
 
-    Model.User.all({ order: [ [ 'username', 'ASC' ] ] }).then((users) => {
+    Model.User.all({ order: [ [ 'createdAt', 'DESC' ] ] }).then((users) => {
+
       res.status(200).json(users);
+
+    });
+
+  }
+
+  static create (req, res) {
+
+    Model.User.create({
+
+      username: req.body.username,
+      password: req.body.password
+
+    }).then((user) => {
+
+      res.status(201).json({
+        message: 'user successfully created',
+        user: user
+      });
+
     });
 
   }
 
   static getById (req, res) {
+
     Model.User.findById(req.params.id).then((user) => {
+
       res.status(200).json(user);
+
     });
+
+  }
+
+  static deleteById (req, res) {
+
+    Model.User.destroy({
+
+      where: { id: req.params.id }
+
+    }).then((user) => {
+
+      res.status(200).json({
+        message: 'user successfully deleted'
+      });
+
+    });
+
+  }
+
+  static updateById (req, res) {
+
+    Model.User.update(
+      {
+        username: req.body.username,
+        password: req.body.password
+      },
+      {
+        where: { id: req.params.id },
+        individualHooks: true
+      }
+    ).then((user) => {
+
+      res.status(200).json({
+        message: 'user successfully updated'
+      });
+
+    });
+
   }
 
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,9 @@ var router = express.Router();
 const UserController = require('../../controllers/UserController');
 
 router.get('/', UserController.getAll);
+router.post('/', UserController.create);
 router.get('/:id', UserController.getById);
+router.delete('/:id', UserController.deleteById);
+router.put('/:id', UserController.updateById);
 
 module.exports = router;
