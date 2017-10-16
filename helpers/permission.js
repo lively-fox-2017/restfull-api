fda60de1251c5cd1eba1ac1f1734253cb265bc32
NNcrawler <rasyid.xyz@gmail.com>
2017-10-16 19:09:35 +0700

    ready to deploy

---
+++ helpers/permission.js
@@ -0,0 +1,40 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+
+function parseToken(req, res, callback){
+  let token =  req.headers.token;
+  jwt.verify(req.headers.token, process.env.saltForToken, function(err, decoded){
+    if(err){
+      res.status(403).json({message:'Forbidden access',data:{}});
+    }
+    callback(decoded)
+  })
+}
+
+class permission{
+  static allowedAdmin(req, res, next){
+    parseToken(req, res, function(decoded){
+      if (decoded.role){
+        if (decoded.role=='admin'){
+          next()
+        }else{
+          res.status(403).json({message:'Forbidden Access'})
+        }
+      }
+    })
+
+  }
+
+  static allowedUser(req, res, next){
+    parseToken(req, res, function(decoded){
+      if(decoded.username){
+        next()
+      }else{
+        res.status(403).json({message:'Forbidden Access'})
+      }
+    })
+
+  }
+}
+
+module.exports = permission;
