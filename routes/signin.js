131fc8606085119e3a3c7e68f42a4e92284253e7
Kautzar Alibani <kalibani.ka@gmail.com>
2017-10-16 20:27:53 +0700

    Revert "done"

--- routes/signin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const data = require('../controllers/users');
-
-router.post('/', data.signin)
-// router.get('/', data.validate)
-
-module.exports = router;
--- routes/signup.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const data = require('../controllers/users');
-const router = express.Router();
-
-
-router.post('/', data.signup)
-
-module.exports = router;
