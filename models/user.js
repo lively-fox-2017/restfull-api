be9bc9d8e2604e00b3bab7fa923979f322fd6929
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-16 18:59:23 +0700

    add not null and not empty validation

--- models/user.js
+++ models/user.js
@@ -5,14 +5,31 @@ module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     username: {
     	type: DataTypes.STRING,
+      allowNull: false,
     	unique: {
     		args: true,
     		msg: 'Validation Error: Username is already used'
-    	}
+    	},
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Validation Error: Username can not be empty'
+        }
+      }
     },
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Validation Error: Password can not be empty'
+        }
+      }
+    },
     role: {
       type: DataTypes.STRING,
+      allowNull: false,
       validate: {
         isIn: {
           args: [['admin', 'user']],
